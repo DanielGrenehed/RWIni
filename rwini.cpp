1cfb43071eda56fa9ace172b475882dcc70f55ee
Weird-Dan <t4.danie.grenehed@umea.nti.se>
2017-04-07 19:15:04 +0200

    out-of-range fix
    Fixed out-of-range exception when a value for a key is empty

--- rwini.cpp
+++ rwini.cpp
@@ -14,13 +14,13 @@
 #include "rwini.hpp"
 
 /*
- 
+
  inits module, returns true if the given filename is an .ini file
- 
+
  */
 bool RWIni::init(std::string filename) {
     this->filename = filename;
-    
+
     std::string ini = std::string(".ini");
     if (filename.length() >= ini.length()) {
         return (0 == filename.compare(filename.length() - ini.length(), filename.length(), ini));
@@ -29,9 +29,9 @@ bool RWIni::init(std::string filename) {
 }
 
 /*
- 
+
  searches the ini file for key in section and returns the value of the key as a result
- 
+
  */
 std::string RWIni::getValue(std::string section, std::string key) {
     std::string tmp, result="", ls, sec = std::string("[") + toLower(section) + std::string("]");
@@ -39,9 +39,9 @@ std::string RWIni::getValue(std::string section, std::string key) {
     if (!file | !file.is_open()) return "";
     bool right_section = false;
     while (std::getline(file, ls)) {
-        
+
         if (isComment(ls)) continue;
-        
+
         if (right_section) {
             if (isSection(ls)) right_section = false;
             else {
@@ -55,15 +55,15 @@ std::string RWIni::getValue(std::string section, std::string key) {
             }
         }
     }
-    
+
     file.close();
     return result;
 }
 
 /*
- 
+
  tries to get the value of the key in section and sets result to value, returns false if it failes to find value for key
- 
+
  */
 bool RWIni::getValue_as_String(std::string section, std::string key, std::string & result) {
     std::string val = getValue(section, key);
@@ -73,9 +73,9 @@ bool RWIni::getValue_as_String(std::string section, std::string key, std::string
 }
 
 /*
- 
+
   tries to get the value of the key in section and sets result to value, returns false if it failes to find value for key or cannot convert value to int
- 
+
  */
 bool RWIni::getValue_as_Int(std::string section, std::string key, int & result) {
     std::string val = getValue(section, key);
@@ -86,9 +86,9 @@ bool RWIni::getValue_as_Int(std::string section, std::string key, int & result) 
 }
 
 /*
- 
+
   tries to get the value of the key in section and sets result to value, returns false if it failes to find value for key or cannot convert value to long
- 
+
  */
 bool RWIni::getValue_as_Long(std::string section, std::string key, long & result) {
     std::string val = getValue(section, key);
@@ -99,9 +99,9 @@ bool RWIni::getValue_as_Long(std::string section, std::string key, long & result
 }
 
 /*
- 
+
   tries to get the value of the key in section and sets result to value, returns false if it failes to find value for key or cannot convert value to float
- 
+
  */
 bool RWIni::getValue_as_Float(std::string section, std::string key, float & result) {
     std::string val = getValue(section, key);
@@ -112,9 +112,9 @@ bool RWIni::getValue_as_Float(std::string section, std::string key, float & resu
 }
 
 /*
- 
+
   tries to get the value of the key in section and sets result to value, returns false if it failes to find value for key or cannot convert value to double
- 
+
  */
 bool RWIni::getValue_as_Double(std::string section, std::string key, double & result) {
     std::string val = getValue(section, key);
@@ -125,9 +125,9 @@ bool RWIni::getValue_as_Double(std::string section, std::string key, double & re
 }
 
 /*
- 
+
   tries to get the value of the key in section and sets result to value, returns false if it failes to find value for key or cannot convert value to bool
- 
+
  */
 bool RWIni::getValue_as_Bool(std::string section, std::string key, bool & result) {
     std::string val = getValue(section, key);
@@ -155,33 +155,33 @@ bool RWIni::getValue_as_Bool(std::string section, std::string key, bool & result
 
 
 /*
- 
+
  Opens the ini file, searches for the key and sets its value to value
  if key isn't found, it adds the key=value to the end of the section
  if section isn't found, it adds the section to the end of the file and adds key=value the next line.
- 
+
  */
 bool RWIni::setValue(std::string section, std::string key, std::string value) {
     // set value of key in section in ini-file
     std::string ls, sec = std::string("[") + toLower(section) + std::string("]");
-    
+
      std::vector<std::string> buffr;
-    
+
     if (iniExists()) { // ini exists, read and modify content
-    
+
         std::ifstream file(this->filename.c_str());
         if (!file) return false;
-    
-   
+
+
         bool right_section = false;
         bool value_set = false;
         while (std::getline(file, ls)) {
-        
+
         // find section
-        
+
             if (isComment(ls)) {
-           
-            
+
+
             } else if (right_section) {
             // check if its new section
                 if (isSection(ls)) {
@@ -190,7 +190,7 @@ bool RWIni::setValue(std::string section, std::string key, std::string value) {
                         buffr.push_back(key+"="+value);
                         value_set = true;
                     }
-                
+
                 } else {
                     // check if line starts with key=
                     if (isKey(ls, key) && !value_set) {// key found, replace line with key=value
@@ -199,18 +199,18 @@ bool RWIni::setValue(std::string section, std::string key, std::string value) {
                         continue;
                     }
                 }
-            
+
             } else {
                 if (trim(toLower(ls)) == sec) {
                     right_section = true;
                 }
             }
-        
+
             buffr.push_back(ls);
         }
-    
+
         file.close();
-    
+
         if (!value_set) {
             if (right_section) {
                 buffr.push_back(key+"="+value);
@@ -225,25 +225,25 @@ bool RWIni::setValue(std::string section, std::string key, std::string value) {
         buffr.push_back(key+"="+value);
     }
     // write buffr to file
-    
+
     std::ofstream wfile(this->filename);
     //wfile.open(this->filename, std::ofstream::out | std::ofstream::trunc); //open and discard all text in the file
-    
+
     if (!wfile) return false;
-    
+
     for (size_t i =0;  i < buffr.size(); i++) {
         wfile << buffr.at(i) << '\n';
     }
-    
+
     wfile.close();
-    
+
     return true;
 }
 
 /*
- 
+
  test if able to access the file
- 
+
  */
 bool RWIni::iniExists() {
     std::ifstream f(this->filename);
@@ -254,9 +254,9 @@ bool RWIni::iniExists() {
 
 
 /*
- 
+
  returns the filepath of the RWIni object
- 
+
  */
 std::string RWIni::getIniFilename() {
     return this->filename;
@@ -276,9 +276,9 @@ std::string RWIni::getIniFilename() {
 
 
 /*
- 
+
  returns the location of the first character that is not space of string
- 
+
  */
 size_t RWIni::getFirstChar(const std::string & str) {
     return str.find_first_not_of(' ');
@@ -289,20 +289,20 @@ bool RWIni::isKey(const std::string & s, const std::string & key) {
     size_t start = getFirstChar(s);
     if (trim(toLower(s.substr(start, start+key.length()))) != key) return false;
     // first word is equal to key
-    
+
     size_t p =  s.find('=');
     size_t fnos = s.find_first_not_of(' ', start+key.length());
-    
+
     if (p == std::string::npos || p != fnos) {
         return false;
     }
     return true;
 }
 
-/* 
- 
+/*
+
  returns true if the given string is a section
- 
+
  */
 bool RWIni::isSection(const std::string &s) {
     size_t f = s.find_first_not_of(' ');
@@ -314,9 +314,9 @@ bool RWIni::isSection(const std::string &s) {
 }
 
 /*
- 
+
  returns true if the given string is a section
- 
+
  */
 bool RWIni::isComment(const std::string &s) {
     size_t hc = s.find(std::string("#"));
@@ -329,30 +329,31 @@ bool RWIni::isComment(const std::string &s) {
 }
 
 /*
- 
+
  returns value for key if it exists else it returns ""
- 
+
  */
 std::string RWIni::value_for_key(const std::string & s, const std::string key) {
     size_t start = getFirstChar(s);
     if (trim(toLower(s.substr(start, start+key.length()))) != key) return "";
     // first word is equal to key
-    
+
     size_t p =  s.find('=');
     size_t fnos = s.find_first_not_of(' ', start+key.length());
-    
+
     if (p == std::string::npos || p != fnos) {
         return "";
     }
-    
+
     p = s.find_first_not_of(" \t\n\r\0", p+1);
+    if (p == std::string::npos) return "";
     return s.substr(p);
 }
 
 /*
- 
+
  removes some characters
- 
+
  */
 std::string RWIni::trim(std::string s) {
     size_t l = s.length();
@@ -371,9 +372,9 @@ std::string RWIni::trim(std::string s) {
 }
 
 /*
- 
+
  returns the same string with all characters as lowercase
- 
+
  */
 std::string RWIni::toLower(const std::string & s) {
     std::string str = s;
@@ -382,8 +383,3 @@ std::string RWIni::toLower(const std::string & s) {
     }
     return str;
 }
-
-
-
-
-
